f45cf07fd991453b4899127548bf10343cc59dad
mary-dalrymple <mary.s.dalrymple@gmail.com>
2022-04-23 21:52:57 -0400

    updates db tables, bot commands

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from slack.errors import SlackApiError
 from sqlite_utils import Database
 
 db = Database("hipaabreach.db")
-record = db.query("""SELECT * from breaches limit 1""")
-settled = db.query("""SELECT * from breach_archive limit 1""")
-ransom = db.query("""SELECT * from ransomware limit 1""")
+record = db.query("""SELECT * from new_or_updated WHERE "Web Description" = 'NA' limit 3 """)
+ransom = db.query("""SELECT * from new_or_updated WHERE "Web Description" like "%ransomware%" limit 1 """)
+settled = db.query("""SELECT * from new_or_updated WHERE "Web Description" != 'NA' limit 3""")
 #for row in record:
 #  print(record)
 
@@ -26,19 +26,19 @@ try:
   for row in record:
     response = client.chat_postMessage(
       channel="slack-bots",
-      text=f"New breach reported by {row['Name of Covered Entity']} on {row['Breach Submission Date']} -- {row['Type of Breach']} affected {row['Individuals Affected']} people."
+      text=f"New breach reported by *{row['Name of Covered Entity']}* on {row['Breach Submission Date']} -- {row['Type of Breach']} affected *{row['Individuals Affected']}* people."
     )
 
     for row in settled:
         response = client.chat_postMessage(
         channel = "slack-bots",
-        text=f"{row['Name of Covered Entity']} breach (reported {row['Breach Submission Date']}, {row['Individuals Affected']} people affected) resolved: \n> {row['Web Description']}"
+        text=f"*{row['Name of Covered Entity']}* breach (reported {row['Breach Submission Date']}, *{row['Individuals Affected']} people affected*) resolved: \n> {row['Web Description']}"
         )
 
     for row in ransom:
         response = client.chat_postMessage(
         channel = "slack-bots",
-        text=f":pirate_flag: Latest ransomware news! {row['Name of Covered Entity']}, {row['Individuals Affected']} people affected \npython > {row['Web Description']}"
+        text=f":pirate_flag: Latest ransomware update! *{row['Name of Covered Entity']}* (reported {row['Breach Submission Date']}) *{row['Individuals Affected']} people affected* \n> {row['Web Description']}"
         )
 
 
